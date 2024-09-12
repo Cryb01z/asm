7fc452be3256baf4e7e07f07a5e521a60d43f081
Cryb01z <anhphong01991@gmail.com>
2024-09-12 22:31:27 +0700

    test

--- src/pages/CVE/CVE.jsx
+++ src/pages/CVE/CVE.jsx
@@ -19,6 +19,49 @@ const CVE = () => {
       reference: "",
     },
   ]);
+
+  const data = [
+    {
+      id: "CVE-2024-8655",
+      vendor: "",
+      product: "",
+      update: "2024-09-12",
+      score: "5.3 Medium",
+      discription: "",
+    },
+    {
+      id: "CVE-2024-8655",
+      vendor: "",
+      product: "",
+      update: "2024-09-12",
+      score: "5.3 Medium",
+      discription: "",
+    },
+    {
+      id: "CVE-2024-8655",
+      vendor: "",
+      product: "",
+      update: "2024-09-12",
+      score: "5.3 Medium",
+      discription: "",
+    },
+    {
+      id: "CVE-2024-8655",
+      vendor: "",
+      product: "",
+      update: "2024-09-12",
+      score: "5.3 Medium",
+      discription: "",
+    },
+    {
+      id: "CVE-2024-8655",
+      vendor: "",
+      product: "",
+      update: "2024-09-12",
+      score: "5.3 Medium",
+      discription: "",
+    },
+  ];
   const searchParams = new URLSearchParams(window.location.search);
   const service = searchParams.get("name");
   useEffect(() => {
@@ -107,17 +150,17 @@ const CVE = () => {
               </div>
             </div>
             <div className="flex mt-4 space-x-10">
-              <div className="flex p-4 border-t-4 rounded-sm border-blue-700 bg-slate-800 w-[900px]">
+              <div className="flex space-x-7 p-4 border-t-4 rounded-sm border-blue-700 bg-slate-800 w-[900px]">
                 <select
                   id="countries"
-                  class="border text-sm rounded-sm block p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 focus:ring-blue-500 focus:border-blue-500"
+                  class="w-48 border text-sm rounded-sm block p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 focus:ring-blue-500 focus:border-blue-500"
                 >
                   <option selected>Select a tag</option>
                   <option value="US">No results found</option>
                 </select>
                 <select
                   id="countries"
-                  class="border text-sm rounded-sm block p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 focus:ring-blue-500 focus:border-blue-500"
+                  class="w-48 border text-sm rounded-sm block p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 focus:ring-blue-500 focus:border-blue-500"
                 >
                   <option selected>Empty</option>
                   <option value="CA">Low (0 - 3.9)</option>
@@ -127,7 +170,7 @@ const CVE = () => {
                 </select>
                 <input
                   type="text"
-                  className="border text-sm rounded-sm block p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 focus:ring-blue-500 focus:border-blue-500"
+                  className="w-60 border text-sm rounded-sm block p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 focus:ring-blue-500 focus:border-blue-500"
                   placeholder="Search in CVEs"
                 />
                 <div className="ml-10 border text-sm rounded-sm flex items-center justify-center px-4 bg-blue-600 hover:bg-blue-700 cursor-pointer">
@@ -142,6 +185,271 @@ const CVE = () => {
                   TOTAL <br />
                   <span className="font-bold">262540 CVE</span>
                 </div>
+              </div>
+            </div>
+            <table className="table-auto bg-slate-800 mt-4 w-[1230px]">
+              <thead className="border-t-4 border-blue-700 font-bold">
+                <tr>
+                  <th className="p-2 text-left w-56">CVE</th>
+                  <th className="p-2 text-left w-80">Vendors</th>
+                  <th className="p-2 text-left w-80">Products</th>
+                  <th className="p-2 w-48">Updated</th>
+                  <th className="p-2 w-44">CVSS v3.1</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr className="border-t-2 border-gray-500">
+                  <td className="p-2 text-left text-blue-600">CVE-2024-8655</td>
+                  <td className="p-2 text-left"></td>
+                  <td className="p-2 text-left"></td>
+                  <td className="p-2 text-center">2024-09-12</td>
+                  <td className="p-2 text-center">
+                    <span className="px-2 text-sm rounded-sm bg-orange-500">
+                      5.3 Medium
+                    </span>
+                  </td>
+                </tr>
+                <tr>
+                  <td className="p-2 text-sm" colSpan={5}>
+                    A vulnerability was found in Mercury MNVR816 up to
+                    2.0.1.0.5. It has been classified as problematic. This
+                    affects an unknown part of the file /web-static/. The
+                    manipulation leads to files or directories accessible. It is
+                    possible to initiate the attack remotely. The exploit has
+                    been disclosed to the public and may be used. The vendor was
+                    contacted early about this disclosure but did not respond in
+                    any way.
+                  </td>
+                </tr>
+                <tr className="border-t-2 border-gray-500">
+                  <td className="p-2 text-left text-blue-600">CVE-2024-8655</td>
+                  <td className="p-2 text-left"></td>
+                  <td className="p-2 text-left"></td>
+                  <td className="p-2 text-center">2024-09-12</td>
+                  <td className="p-2 text-center">
+                    <span className="px-2 text-sm rounded-sm bg-orange-500">
+                      5.3 Medium
+                    </span>
+                  </td>
+                </tr>
+                <tr>
+                  <td className="p-2 text-sm" colSpan={5}>
+                    A vulnerability was found in Mercury MNVR816 up to
+                    2.0.1.0.5. It has been classified as problematic. This
+                    affects an unknown part of the file /web-static/. The
+                    manipulation leads to files or directories accessible. It is
+                    possible to initiate the attack remotely. The exploit has
+                    been disclosed to the public and may be used. The vendor was
+                    contacted early about this disclosure but did not respond in
+                    any way.
+                  </td>
+                </tr>
+                <tr className="border-t-2 border-gray-500">
+                  <td className="p-2 text-left text-blue-600">CVE-2024-8655</td>
+                  <td className="p-2 text-left"></td>
+                  <td className="p-2 text-left"></td>
+                  <td className="p-2 text-center">2024-09-12</td>
+                  <td className="p-2 text-center">
+                    <span className="px-2 text-sm rounded-sm bg-orange-500">
+                      5.3 Medium
+                    </span>
+                  </td>
+                </tr>
+                <tr>
+                  <td className="p-2 text-sm" colSpan={5}>
+                    A vulnerability was found in Mercury MNVR816 up to
+                    2.0.1.0.5. It has been classified as problematic. This
+                    affects an unknown part of the file /web-static/. The
+                    manipulation leads to files or directories accessible. It is
+                    possible to initiate the attack remotely. The exploit has
+                    been disclosed to the public and may be used. The vendor was
+                    contacted early about this disclosure but did not respond in
+                    any way.
+                  </td>
+                </tr>
+                <tr className="border-t-2 border-gray-500">
+                  <td className="p-2 text-left text-blue-600">CVE-2024-8655</td>
+                  <td className="p-2 text-left"></td>
+                  <td className="p-2 text-left"></td>
+                  <td className="p-2 text-center">2024-09-12</td>
+                  <td className="p-2 text-center">
+                    <span className="px-2 text-sm rounded-sm bg-orange-500">
+                      5.3 Medium
+                    </span>
+                  </td>
+                </tr>
+                <tr>
+                  <td className="p-2 text-sm" colSpan={5}>
+                    A vulnerability was found in Mercury MNVR816 up to
+                    2.0.1.0.5. It has been classified as problematic. This
+                    affects an unknown part of the file /web-static/. The
+                    manipulation leads to files or directories accessible. It is
+                    possible to initiate the attack remotely. The exploit has
+                    been disclosed to the public and may be used. The vendor was
+                    contacted early about this disclosure but did not respond in
+                    any way.
+                  </td>
+                </tr>
+                <tr className="border-t-2 border-gray-500">
+                  <td className="p-2 text-left text-blue-600">CVE-2024-8655</td>
+                  <td className="p-2 text-left"></td>
+                  <td className="p-2 text-left"></td>
+                  <td className="p-2 text-center">2024-09-12</td>
+                  <td className="p-2 text-center">
+                    <span className="px-2 text-sm rounded-sm bg-orange-500">
+                      5.3 Medium
+                    </span>
+                  </td>
+                </tr>
+                <tr>
+                  <td className="p-2 text-sm" colSpan={5}>
+                    A vulnerability was found in Mercury MNVR816 up to
+                    2.0.1.0.5. It has been classified as problematic. This
+                    affects an unknown part of the file /web-static/. The
+                    manipulation leads to files or directories accessible. It is
+                    possible to initiate the attack remotely. The exploit has
+                    been disclosed to the public and may be used. The vendor was
+                    contacted early about this disclosure but did not respond in
+                    any way.
+                  </td>
+                </tr>
+                <tr className="border-t-2 border-gray-500">
+                  <td className="p-2 text-left text-blue-600">CVE-2024-8655</td>
+                  <td className="p-2 text-left"></td>
+                  <td className="p-2 text-left"></td>
+                  <td className="p-2 text-center">2024-09-12</td>
+                  <td className="p-2 text-center">
+                    <span className="px-2 text-sm rounded-sm bg-orange-500">
+                      5.3 Medium
+                    </span>
+                  </td>
+                </tr>
+                <tr>
+                  <td className="p-2 text-sm" colSpan={5}>
+                    A vulnerability was found in Mercury MNVR816 up to
+                    2.0.1.0.5. It has been classified as problematic. This
+                    affects an unknown part of the file /web-static/. The
+                    manipulation leads to files or directories accessible. It is
+                    possible to initiate the attack remotely. The exploit has
+                    been disclosed to the public and may be used. The vendor was
+                    contacted early about this disclosure but did not respond in
+                    any way.
+                  </td>
+                </tr>
+                <tr className="border-t-2 border-gray-500">
+                  <td className="p-2 text-left text-blue-600">CVE-2024-8655</td>
+                  <td className="p-2 text-left"></td>
+                  <td className="p-2 text-left"></td>
+                  <td className="p-2 text-center">2024-09-12</td>
+                  <td className="p-2 text-center">
+                    <span className="px-2 text-sm rounded-sm bg-orange-500">
+                      5.3 Medium
+                    </span>
+                  </td>
+                </tr>
+                <tr>
+                  <td className="p-2 text-sm" colSpan={5}>
+                    A vulnerability was found in Mercury MNVR816 up to
+                    2.0.1.0.5. It has been classified as problematic. This
+                    affects an unknown part of the file /web-static/. The
+                    manipulation leads to files or directories accessible. It is
+                    possible to initiate the attack remotely. The exploit has
+                    been disclosed to the public and may be used. The vendor was
+                    contacted early about this disclosure but did not respond in
+                    any way.
+                  </td>
+                </tr>
+                <tr className="border-t-2 border-gray-500">
+                  <td className="p-2 text-left text-blue-600">CVE-2024-8655</td>
+                  <td className="p-2 text-left"></td>
+                  <td className="p-2 text-left"></td>
+                  <td className="p-2 text-center">2024-09-12</td>
+                  <td className="p-2 text-center">
+                    <span className="px-2 text-sm rounded-sm bg-orange-500">
+                      5.3 Medium
+                    </span>
+                  </td>
+                </tr>
+                <tr>
+                  <td className="p-2 text-sm" colSpan={5}>
+                    A vulnerability was found in Mercury MNVR816 up to
+                    2.0.1.0.5. It has been classified as problematic. This
+                    affects an unknown part of the file /web-static/. The
+                    manipulation leads to files or directories accessible. It is
+                    possible to initiate the attack remotely. The exploit has
+                    been disclosed to the public and may be used. The vendor was
+                    contacted early about this disclosure but did not respond in
+                    any way.
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+            <div className="bg-slate-800 w-[1230]">
+              <div className="flex items-center justify-center gap-4">
+                <button
+                  className="flex items-center gap-2 px-6 py-3 font-sans text-xs font-bold text-center text-white uppercase align-middle transition-all rounded-full select-none hover:bg-gray-700 active:bg-gray-900/20 disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+                  type="button"
+                  onClick={prevPage}
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth="2"
+                    stroke="currentColor"
+                    aria-hidden="true"
+                    className="w-4 h-4"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18"
+                    ></path>
+                  </svg>
+                  Previous
+                </button>
+                <div className="flex items-center gap-2">
+                  {numbers.map((n, i) => (
+                    <button
+                      className={
+                        currentPage === n
+                          ? "relative h-10 max-h-[40px] bg-gray-900 w-10 max-w-[40px] select-none rounded-full text-center align-middle font-sans text-xs font-medium uppercase text-white transition-all hover:bg-gray-700 active:bg-gray-900/20 disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+                          : "relative h-10 max-h-[40px] w-10 max-w-[40px] select-none rounded-full text-center align-middle font-sans text-xs font-medium uppercase text-white transition-all hover:bg-gray-700 active:bg-gray-900/20 disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+                      }
+                      type="button"
+                      key={i}
+                      onClick={() => {
+                        changePage(n);
+                      }}
+                    >
+                      <span className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2">
+                        {n}
+                      </span>
+                    </button>
+                  ))}
+                </div>
+                <button
+                  className="flex items-center gap-2 px-6 py-3 font-sans text-xs font-bold text-center text-white uppercase align-middle transition-all rounded-full select-none hover:bg-gray-700 active:bg-gray-900/20 disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+                  type="button"
+                  onClick={nextPage}
+                >
+                  Next
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth="2"
+                    stroke="currentColor"
+                    aria-hidden="true"
+                    className="w-4 h-4"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3"
+                    ></path>
+                  </svg>
+                </button>
               </div>
             </div>
             {/*<div className="flex justify-between py-5">
